709cd82478062232548385047af5084ac53205fb
dan-acsc <dan-acsc@users.noreply.github.com>
2026-02-26 03:09:29 +0000

    trial json after

--- tests/integration/restapi/binary2/test_binary_find_paginate.py
+++ tests/integration/restapi/binary2/test_binary_find_paginate.py
@@ -7,12 +7,6 @@ from tests.support import gen, integration_test
 class TestBinaryFind(integration_test.BaseRestapi):
     def test_binary_find_simple(self):
         self.write_binary_events([gen.binary_event(eid=f"e{x}", authornv=("a1", "1")) for x in range(210)])
-        # Extra refreshes to ensure it's ready.
-        time.sleep(0.1)
-        self.writer.refresh()
-        time.sleep(1)
-        self.writer.refresh()
-
         response = self.client.post("/v0/binaries/all?num_binaries=100")
         self.assertEqual(200, response.status_code)
         parsed = response.json()
@@ -23,7 +17,7 @@ class TestBinaryFind(integration_test.BaseRestapi):
 
         print("after", after)
         print(parsed)
-        response = self.client.post("/v0/binaries/all?num_binaries=100", content=json.dumps({"after": after}))
+        response = self.client.post("/v0/binaries/all?num_binaries=100", json={"after": after})
         print(response.content)
 
         self.assertEqual(200, response.status_code)
@@ -33,7 +27,7 @@ class TestBinaryFind(integration_test.BaseRestapi):
         self.assertEqual(after, '{"SHA256": "e9"}')
         self.assertNotIn("total", parsed["data"])
 
-        response = self.client.post("/v0/binaries/all?num_binaries=100", content=json.dumps({"after": after}))
+        response = self.client.post("/v0/binaries/all?num_binaries=100", json={"after": after})
         self.assertEqual(200, response.status_code)
         parsed = response.json()
         self.assertEqual(10, len(parsed["data"]["items"]))
@@ -41,7 +35,7 @@ class TestBinaryFind(integration_test.BaseRestapi):
         self.assertEqual(after, '{"SHA256": "e99"}')
         self.assertNotIn("total", parsed["data"])
 
-        response = self.client.post("/v0/binaries/all?num_binaries=100", content=json.dumps({"after": after}))
+        response = self.client.post("/v0/binaries/all?num_binaries=100", json={"after": after})
         self.assertEqual(200, response.status_code)
         parsed = response.json()
         self.assertEqual(0, len(parsed["data"]["items"]))
@@ -56,11 +50,6 @@ class TestBinaryFind(integration_test.BaseRestapi):
         self.assertEqual(parsed["data"]["total"], 210)
 
     def test_binary_find_free_text(self):
-        # Extra refreshes to ensure it's ready.
-        time.sleep(0.1)
-        self.writer.refresh()
-        time.sleep(1)
-        self.writer.refresh()
         self.write_binary_events(
             [
                 gen.binary_event(eid=f"e1{x}", authornv=("a1", "1"), magicmime=("", "application/zip"))
@@ -86,7 +75,7 @@ class TestBinaryFind(integration_test.BaseRestapi):
         response = self.client.post(
             "/v0/binaries/all?num_binaries=100",
             params={"term": '"application/zip"'},
-            content=json.dumps({"after": after}),
+            json={"after": after},
         )
         print(response.content)
         self.assertEqual(200, response.status_code)
