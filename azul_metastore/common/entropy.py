f05eb993fd99822d4b79c3238b6d2855f3321aea
dan-acsc <dan-acsc@users.noreply.github.com>
2026-03-18 00:52:26 +0000

    even more precsion is now available.

--- azul_metastore/common/entropy.py
+++ azul_metastore/common/entropy.py
@@ -1,12 +1,21 @@
 """Encoding methods used to standardise entropy into a knn vector."""
 
+import functools
+
 import numpy as np
 
 # Note - 40 is somewhat arbitrary, keeping this number smaller improves query efficiency
 # The largest value that makes sense is 800 (because there are at most 800 values in entropy with the way the entropy plugin works)
 # 40 was selected because 800 divides into it equally but it wasn't so large it would cause problems
 ENTROPY_VECTOR_DIMENSION = 40
-TOTAL_ENTROPY_BITS = ENTROPY_VECTOR_DIMENSION * 8
+# should be factor of 2 (as this is the number of bytes to store in opensearch)
+# Precision is how much entropy is rounded to the nearest 1/PRECISION_FACTOR, so 4 means 1/4==0.25, 1/8==0.125 etc...
+PRECISION_FACTOR = 4
+# Number of bits to store each float in opensearch
+NUMBER_OF_BITS = 8 * PRECISION_FACTOR
+# Total number of bits to store in opensearch
+TOTAL_ENTROPY_BITS = ENTROPY_VECTOR_DIMENSION * NUMBER_OF_BITS
+# Entropy maximum and minimum values.
 MAX_ENTROPY_VALUE = 8.0
 MIN_ENTROPY_VALUE = 0.0
 
@@ -38,29 +47,52 @@ def _convert_bit_sequence_to_number(bit_sequence: list[int]) -> int:
     return int(signed_int)
 
 
-# Convert an integer value to the appropriate binary representation
-_int_conversion_table = {
-    0: _convert_bit_sequence_to_number([0, 0, 0, 0, 0, 0, 0, 0]),  # 0
-    1: _convert_bit_sequence_to_number([0, 0, 0, 0, 0, 0, 0, 1]),  # 1
-    2: _convert_bit_sequence_to_number([0, 0, 0, 0, 0, 0, 1, 1]),  # 3
-    3: _convert_bit_sequence_to_number([0, 0, 0, 0, 0, 1, 1, 1]),  # 7
-    4: _convert_bit_sequence_to_number([0, 0, 0, 0, 1, 1, 1, 1]),  # 15
-    5: _convert_bit_sequence_to_number([0, 0, 0, 1, 1, 1, 1, 1]),  # 31
-    6: _convert_bit_sequence_to_number([0, 0, 1, 1, 1, 1, 1, 1]),  # 63
-    7: _convert_bit_sequence_to_number([0, 1, 1, 1, 1, 1, 1, 1]),  # 127
-    8: _convert_bit_sequence_to_number([1, 1, 1, 1, 1, 1, 1, 1]),  # -1
-}
+def _round_to_precision(value: float):
+    """Rounds to the appropriate precision level."""
+    precision = 1 / PRECISION_FACTOR
+    return round(value / precision) * precision
+
+
+@functools.cache
+def _get_int_conversion_table() -> dict[float, list[int]]:
+    """Get a cache to convert an arbitrary float into a bit sequence based on the size of the float.
+
+    A larger float value will get a corresponding number of 1 bits that is larger.
+    The bits fill left to right, so the numerical value will gradually increase the larger the float.
+    """
+    result: dict[float, list[int]] = dict()
+    step_size = MAX_ENTROPY_VALUE / (8 * PRECISION_FACTOR)
+    total_steps = int(MAX_ENTROPY_VALUE * PRECISION_FACTOR)
+    for cur_step in range(total_steps + 1):
+        result[_round_to_precision(cur_step * step_size)] = []
+        total_number_of_ones = cur_step
+        for _ in range(PRECISION_FACTOR):
+            if total_number_of_ones >= 8:
+                # More than 8 ones
+                bit_sequence = [1] * 8
+                total_number_of_ones -= 8
+            elif total_number_of_ones > 0:
+                # Less than 8 ones
+                ones_for_iteration = total_number_of_ones % 8
+                bit_sequence = [1] * ones_for_iteration + [0] * (8 - ones_for_iteration)
+                total_number_of_ones = 0
+            else:
+                # pad the rest with 0.
+                bit_sequence = [0] * 8
+            result[_round_to_precision(cur_step * step_size)].append(_convert_bit_sequence_to_number(bit_sequence))
+    return result
 
 
 def _convert_float_0_8_to_binary(entropy_values: np.ndarray) -> list[np.int8]:
     """Convert an entropy array with values 0->8 to a byte array that is useful for binary comparison.
+
     (values in array range from -128 to 127, but the focus of conversion is on the bit vaules)
     This method allows for hamming bit comparisons in opensearch nearest neighbour searches.
     """
     result = []
     for v in entropy_values:
-        bin_value = _int_conversion_table[int(round(v, 0))]
-        result.append(int(bin_value))
+        bin_values = _get_int_conversion_table()[_round_to_precision(v)]
+        result = result + bin_values
     return result
 
 
