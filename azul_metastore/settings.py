8bc40f18cd88e3f33bcb4e423b4e00ef4e16f7af
dhruv-acsc <dhruv-acsc@users.noreply.github.com>
2026-03-24 02:56:02 +0000

    fix: remove session_id

--- azul_metastore/settings.py
+++ azul_metastore/settings.py
@@ -131,8 +131,7 @@ class Metastore(BaseSettings):
     )
     special_log_message_format: str = (
         'full_time="{time:%d/%b/%Y:%H:%M:%S.%f}" connection="{connection}" username="{username}"'
-        ' special_method="{method}" special_path="{path}" sha256="{sha256}" session_id="{session_id}"'
-        ' action="{action}"'
+        ' special_method="{method}" special_path="{path}" sha256="{sha256}" action="{action}"'
     )
     # File to log special audit events to for loki collection.
     special_log_file_path: str = ""
